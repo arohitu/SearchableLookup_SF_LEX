cf91a26bed1df3438bb929306395013cbc23630e
Rohit <arohitu@gmail.com>
2017-12-28 16:35:43 +0530

    backup

--- src/aura/Generic_Searchable_LookupField/Generic_Searchable_LookupFieldController.js
+++ src/aura/Generic_Searchable_LookupField/Generic_Searchable_LookupFieldController.js
@@ -1,30 +1,30 @@
 ({
-    doInit : function(component, event, helper) {
+    doInit: function(component, event, helper) {
         helper.retrieveRecords(component, event, helper);
     },
-    showAllResults : function(component, event, helper) {
+    showAllResults: function(component, event, helper) {
         var cmpTarget = component.find('recslistdiv');
         $A.util.toggleClass(cmpTarget, 'slds-hide');
     },
-    handleSearchKeyUp : function(component, event, helper) {
+    handleSearchKeyUp: function(component, event, helper) {
         helper.startSearchDisplay(component, event, helper);
     },
-    selectThisRecord : function(component, event, helper) {
+    selectThisRecord: function(component, event, helper) {
         var clickedActivity = event.target.id;
         component.set("v.fieldSelectedVal", clickedActivity);
         var cmpTarget = component.find('recslistdiv');
         $A.util.addClass(cmpTarget, 'slds-hide');
     },
-    checkForNull : function(component,event,helper){
+    checkForNull: function(component, event, helper) {
         var selValue = component.get("v.fieldSelectedVal");
         var errfield = component.find("search-input-error");
-        if(selValue === '')	{
-            errfield.set("v.errors",[{message:"Selezionare un'attività DMS."}]);
+        if (selValue === '') {
+            errfield.set("v.errors", [{ message: "Selezionare un'attività DMS." }]);
             //Do not confirm on parent component if DMS Activity is null
             return false;
-        }else
-        {
-            errfield.set("v.errors",'');
+        } 
+        else {
+            errfield.set("v.errors", '');
             return true;
         }
     }
--- src/aura/Generic_Searchable_LookupField/Generic_Searchable_LookupFieldHelper.js
+++ src/aura/Generic_Searchable_LookupField/Generic_Searchable_LookupFieldHelper.js
@@ -1,41 +1,52 @@
 ({
-    retrieveRecords : function(component, event, helper) {
+    retrieveRecords: function(component, event, helper) {
         var action = component.get("c.retrieveRecords");
         action.setParams({
-            qfields : component.get("v.queryFields"),
-            qObject : component.get("v.fieldObject"),
-            qWhereCond : component.get("v.wherecondition"),
-            lookupField : component.get("v.lookupfieldAPI")
-            
+            qfields: component.get("v.queryFields"),
+            qObject: component.get("v.fieldObject"),
+            qWhereCond: component.get("v.wherecondition"),
+            lookupField: component.get("v.lookupfieldAPI")
+
         });
         action.setCallback(this, function(response) {
             if (response.getState() === "SUCCESS") {
-                component.set("v.retrievedRecords", response.getReturnValue());
-            } 
-            else if (response.getState() === "ERROR") {
+                var retrievedRecs = response.getReturnValue();
+                var finalLookupList = [];
+                var lookupFieldList = [];
+                lookupFieldList.push(component.get("v.lookupfieldAPI"));
+                for (var i = 0; i < retrievedRecs.length; i++) {
+                    for (var ii = 0; ii < lookupFieldList.length; ii++) {
+                        finalLookupList.push(retrievedRecs[i][lookupFieldList[ii]]);
+                    }
+                }
+                component.set("v.retrievedRecords", finalLookupList);
+            } else if (response.getState() === "ERROR") {
                 $A.log("Errors", response.getError());
             }
         });
         $A.enqueueAction(action);
     },
-    startSearchDisplay : function(component, event, helper) {
+    startSearchDisplay: function(component, event, helper) {
         var currentVal, filter, ul, li, a;
         var itemCheck = false;
         currentVal = component.get("v.fieldSelectedVal").toUpperCase();
-        //filter = currentVal.toUpperCase();
         ul = document.getElementById("recsUL");
         li = ul.getElementsByTagName('li');
+        var falseCount = 0;
         // Loop through all list items, and hide those who don't match the search query
         for (var i = 0; i < li.length; i++) {
             a = li[i].getElementsByTagName("a")[0];
             if (a.innerHTML.indexOf(currentVal) > -1) {
                 li[i].style.display = "";
                 itemCheck = true;
-            } else {
+            } 
+            else {
+                falseCount++;
                 li[i].style.display = "none";
             }
         }
-        if(itemCheck === true && currentVal !== ''){
+        console.log('To Hide Items>: '+falseCount);
+        if (itemCheck === true && currentVal !== '') {
             $A.util.removeClass(component.find('recslistdiv'), 'slds-hide');
         }
     }
